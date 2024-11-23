fca67092ab769cf7e994d7d282dd4bed0a53116c
BoRamSon <onbrson1@gmail.com>
2024-11-23 19:17:32 +0900

    feat(InputView): 경주할 자동차 이름을 입력 받는다.

--- src/controller/RacingController.js
+++ src/controller/RacingController.js
@@ -1,6 +1,21 @@
-class RacingController {
-  async racing () {
+import { MissionUtils } from "@woowacourse/mission-utils";
 
+class RacingController {
+  constructor() {}
+
+  async racing() {
+    const carNames = await this.readCarNames();
+
+    return console.log(carNames);
+  }
+
+  // 1. 경주할 자동차 이름을 쉼표(,)기준으로 구분하여 각각 5자 이하로만 입력받는다.
+  async readCarNames() {
+    const inputCarNames = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분) \n => "
+    );
+    const stringType = String(inputCarNames);
+    return stringType;
   }
 }
 

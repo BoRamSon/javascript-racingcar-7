30e255d39c5a2b455ff22b406c79741ed7fb1599
BoRamSon <onbrson1@gmail.com>
2024-11-23 21:26:06 +0900

    feat(controller): 0에서 9 사이에서 무작위 값을 구하는 메서드 생성

--- src/controller/RacingController.js
+++ src/controller/RacingController.js
@@ -43,6 +43,17 @@ class RacingController {
     const validatedValue = validateTryNumber(validationTarget);
     return Number(validationTarget);
   }
+
+  // 5. 게임 진행
+  // 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
+  getRandomNumber() {
+    return MissionUtils.Random.pickNumberInRange(0, 9);
+  }
+
+
+
+
+
 }
 
 export default RacingController;

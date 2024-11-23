be1773b79be1355cde3f6c433e6afa9a21acfa1d
BoRamSon <onbrson1@gmail.com>
2024-11-23 23:37:16 +0900

    fix(validation): import 경로 변경, forEach 코드 변경

--- src/validation/functions/validateFiveLength.js
+++ src/validation/functions/validateFiveLength.js
@@ -1,15 +1,16 @@
 import ERROR_MESSAGES from "../../constants/errorMessages.js";
-import makeArrayFromString from "../../utils/makeArrayFormString.js";
+import makeArrayFromString from "../../utils/makeArrayFromString.js";
 
 const validateFiveLength = (value) => {
   const carNamesArray = makeArrayFromString(value);
-  carNamesArray.map((carName) => {
-    if (carName.length > 5)
+  carNamesArray.forEach((carName) => {
+    if (carName.length > 5) {
       throw new Error(ERROR_MESSAGES.VALIDATE_FIVE_LENGTH);
-    return carName;
+    }
   });
-  
+
   return true;
 };
 
 export default validateFiveLength;
+

df510725fc8ea42bc9f278b8ac2b46dc311ecfc7
BoRamSon <onbrson1@gmail.com>
2024-11-23 20:56:33 +0900

    feat(constants): add errorMessage for number of try

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -2,6 +2,7 @@ const ERROR_MESSAGES = {
   VALIDATE_EMPTY: "[ERROR] 입력값이 없습니다. 다시 입력해주세요.",
   VALIDATE_NOT_CORRECT_REGEX: "[ERROR] 형식이 올바르지 않습니다.",
   VALIDATE_FIVE_LENGTH: "[ERROR] 입력된 자동차 이름 중에서 5글자가 넘는 이름이 존재합니다.",
+  VALIDATE_LIMIT_NUMBER: "[ERROR] 너무 과도한 시도 횟수를 입력하셨습니다.",
 };
 
 export default ERROR_MESSAGES;

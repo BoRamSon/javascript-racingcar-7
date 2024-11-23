0d7efeda0db41ee3b3782e71269a7aac4161c013
BoRamSon <onbrson1@gmail.com>
2024-11-23 23:37:40 +0900

    chore(controller): 주석 제거

--- src/controller/RacingController.js
+++ src/controller/RacingController.js
@@ -15,7 +15,8 @@ class RacingController {
 
   async getCarNamesObject() {
     const carNames = await this.readCarNames();
-    const validatedCarNames = this.validationCarNames(carNames);
+    const stringType = String(carNames);
+    const validatedCarNames = this.validationCarNames(stringType);
     const carNamesArray = makeArrayFromString(validatedCarNames);
     MissionUtils.Console.print("");
     return makeObjectFromArray(carNamesArray);
@@ -28,8 +29,6 @@ class RacingController {
     return Number(validtedTryNumber);
   }
 
-  //-----------------------------------------------------------------
-  // 1. 경주할 자동차 이름을 쉼표(,)기준으로 구분하여 각각 5자 이하로만 입력받는다.
   async readCarNames() {
     const inputCarNames = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분) \n => "
@@ -39,13 +38,11 @@ class RacingController {
     return removeSpace;
   }
 
-  // 2. 입력받은 자동차 이름값들을 검증한다.
   validationCarNames(validationTarget) {
     validateCarNames(validationTarget);
     return validationTarget;
   }
 
-  // 3. 시도할 횟수를 입력받는다.
   async readTryNumbers() {
     const inputCarNames = await MissionUtils.Console.readLineAsync(
       "시도할 횟수는 몇 회인가요? \n => "
@@ -54,19 +51,15 @@ class RacingController {
     return removeSpace;
   }
 
-  // 4. 입력받은 시도할 홧수를 검증한다.
   validationTryNumber(validationTarget) {
     validateTryNumber(validationTarget);
     return validationTarget;
   }
 
-  // 5. 게임 진행
-  // 0에서 9 사이에서 무작위 값을 구한다.
   getRandomNumber() {
     return MissionUtils.Random.pickNumberInRange(0, 9);
   }
 
-  // (forward 숫자를 dashe로 만들어주는 util)
   changeDashUtil(number) {
     let dash = "";
     for (let i = 0; i < number; i++) {
@@ -75,7 +68,6 @@ class RacingController {
     return dash;
   }
 
-  // (print게임진행상황)
   printGameProgress(object) {
     object.map((array) => {
       const carName = array.name;
@@ -85,8 +77,6 @@ class RacingController {
     MissionUtils.Console.print("");
   }
 
-  // 전진하는 조건은 무작위 값이 4 이상일 경우이다. 이 조건으로 게임을 시작한다.
-  // 6. 게임 진행 상황을 출력할 때 자동차 이름을 같이 출력한다.
   startCarRacing(carNamesObject, tryNumber) {
     let racingResult = carNamesObject;
     for (let i = 0; i < tryNumber; i++) {
@@ -99,7 +89,6 @@ class RacingController {
     return racingResult;
   }
 
-  // 7. 우승자 출력하기
   printWinner(carRacingResult) {
     const maxForwardNumber = Math.max(
       ...carRacingResult.map((object) => object.forward)
